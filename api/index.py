8d51f994c70f6f6eeb44ec7b4875b55e9d35c91c
Ritik Pandey <128275361+ritik-pandey527@users.noreply.github.com>
2024-11-01 15:30:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/greet', methods=['GET'])
+def greet():
+    # Check if a name is provided in the request arguments
+    name = request.args.get('name', 'there')
+    greeting_message = f"Hello, {name}!"
+    
+    # Return a JSON response with the greeting
+    return jsonify({"message": greeting_message})
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
